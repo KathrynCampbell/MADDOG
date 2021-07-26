c655b69344a6bc6f9f5107967a94fcb4317a0381
Kathryn Campbell <k.campbell.1@research.gla.ac.uk>
2021-07-26 17:21:31 +0100

    assign lineages functions

--- R/assign_lineages.R
+++ R/assign_lineages.R
@@ -1,24 +1,24 @@
 #' Lineage Assignment
-#' 
+#'
 #' This function tests query sequences against a reference set of sequences and provides a lineage
 #' assignment for the query sequences. It also returns information about each of the lineages assigned.
-#' 
-#' @param sequences The query sequences to provide a lineage assignment for in fasta format
+#'
+#' @param sequences The query sequences to provide a lineage assignment for in DNAbin format
 #' @param reference The reference set to compare the query sequences to (Cosmo_WGS, Cosmo_N)
 #' @return A lineage assignment for each query sequence, and information about that lineage
 #' @export
 assign_lineages<-function(sequences, reference) {
-  alignment<-seqinr::read.alignment(system.file("extdata", paste("References/", reference, "/reference_aligned.fasta", sep = ""), package = "MADDOG"), format = "fasta")
+  ref_align<-seqinr::read.fasta(system.file("extdata", paste("References/", reference, "/reference_aligned.fasta", sep = ""), package = "MADDOG"))
   data <- read.csv(system.file("extdata", paste("References/", reference, "/reference_clusters.csv", sep = ""), package = "MADDOG"))
   clusters <- read.csv(system.file("extdata", paste("References/", reference, "/lineage_info.csv", sep = ""), package = "MADDOG"))
-  
-  #SOMETHING HERE TO ADD SEQUENCES TO ALIGNMENT AND REALIGN
-  
+
+  alignment<-mafft_reorder(sequences, ref_align)
+
   `%notin%` <- Negate(`%in%`)
 
-  test_seqs<-which(alignment$nam %notin% data$ID)
+  test_seqs<-which(row.names(alignment) %notin% data$ID)
 
-  test_seq_assignment<-data.frame(ID=alignment$nam[test_seqs], lineage = NA)
+  test_seq_assignment<-data.frame(ID=row.names(alignment)[test_seqs], lineage = NA)
 
   calculate_mode <- function(x) {
     uniqx<-unique(na.omit(x))
@@ -29,17 +29,17 @@ assign_lineages<-function(sequences, reference) {
   for (i in 1:length(test_seqs)){
     x<-1
     y<-1
-    down<-data$cluster[which(data$ID == alignment$nam[(test_seqs[i]-1)])]
+    down<-data$cluster[which(data$ID == row.names(alignment)[(test_seqs[i]-1)])]
     repeat {
-      down<-data$cluster[which(data$ID == alignment$nam[(test_seqs[i]-(1+x))])]
+      down<-data$cluster[which(data$ID == row.names(alignment)[(test_seqs[i]-(1+x))])]
       x<-x+1
       if (length(down) != 0){
         break
       }
     }
-    up<-data$cluster[which(data$ID == alignment$nam[(test_seqs[i]+1)])]
+    up<-data$cluster[which(data$ID == row.names(alignment)[(test_seqs[i]+1)])]
     repeat {
-      up<-data$cluster[which(data$ID == alignment$nam[(test_seqs[i]+(1+y))])]
+      up<-data$cluster[which(data$ID == row.names(alignment)[(test_seqs[i]+(1+y))])]
       y<-y+1
       if (length(up) != 0){
         break
---
+++ R/mafft_reorder.R
@@ -0,0 +1,61 @@
+#' MAFFT Alignment with Reorder
+#'
+#' This function adds query sequences to a reference alignment and realigns all the sequences, allowing
+#' reordering of the sequences
+#'
+#' @param sequences The query sequences to be added to the reference alignment in fasta format
+#' @param ref_align The reference alignment to add the query sequences to in fasta format
+#' @return An alignment containing the query sequences and reference sequences
+#' @export
+mafft_reorder<-function(sequences, ref_align){
+  os <- .Platform$OS
+
+  exec <- "/usr/local/bin/mafft"
+
+  sequences <- as.list(sequences)
+  fns <- vector(length = 4)
+  for (i in seq_along(fns)) fns[i] <- tempfile(pattern = "mafft",
+                                               tmpdir = tempdir(), fileext = ".fas")
+  unlink(fns[file.exists(fns)])
+
+
+  ref_align <- as.list(ref_align)
+
+
+  ips::write.fas(sequences, fns[1])
+  ips::write.fas(ref_align, fns[2])
+  call.mafft <- paste(exec, " --add ", fns[1], " --reorder ", fns[2], " > ",
+                      fns[3], sep = "")
+
+  if (os == "unix") {
+    system(call.mafft, intern = FALSE, ignore.stdout = FALSE)
+    res <- (file.info(fns[3])$size > 1)
+    if (res != 0) {
+      res <- ape::read.dna(fns[3], format = "fasta")
+    }
+  }
+  else {
+    res <- system(call.mafft, intern = TRUE, ignore.stderr = FALSE)
+    if (length(grep("error|ERROR", res))) {
+      res <- 0
+    }
+    else {
+      res <- ape::read.dna(fns[3], format = "fasta")
+    }
+  }
+  unlink(fns[file.exists(fns)])
+
+  res<-as.character(res)
+
+  return(res)
+
+}
+
+
+
+
+
+
+
+
+
