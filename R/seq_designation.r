b268184e07b0accd85009e8be9fde19b03c866b9
KathrynCampbell <k.campbell.1@research.gla.ac.uk>
2021-12-01 16:25:14 +0000

    Strange name bug... increase from 26 to 32

--- R/seq_designation.r
+++ R/seq_designation.r
@@ -288,7 +288,8 @@ seq_designation <- function(tree, min.support, alignment, metadata, ancestral) {
   node_data<-node_data[order(node_data$overlaps, decreasing = T), ]
   node_data$test <- NA
   problem_names<-data.frame(letters = c("A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1", "K1", "L1", "M1", "N1",
-                                        "O1", "P1", "Q1", "R1", "S1", "T1", "U1", "V1", "W1", "X1", "Y1", "Z1"))
+                                        "O1", "P1", "Q1", "R1", "S1", "T1", "U1", "V1", "W1", "X1", "Y1", "Z1", "AA1", "AB1",
+                                        "AC1", "AD1", "AE1", "AF1"))
   possible_names<-possible_names[order(possible_names$names),]
   possible_names<-paste(possible_names, problem_names$letters, sep = "_")
 
