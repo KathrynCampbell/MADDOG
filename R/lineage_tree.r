f0e561befcdbb93a5634a73a57f794e9999e08b3
KathrynCampbell <k.campbell.1@research.gla.ac.uk>
2022-04-01 19:10:10 +0100

    Test tree update less NA colours

--- R/lineage_tree.r
+++ R/lineage_tree.r
@@ -40,6 +40,9 @@ lineage_tree <- function(lineage_info, node_data, tree, metadata, sequence_data)
 
   clades<-unique(lineages$subclade)
 
+  lineage_info$lineage<-gsub("Asian_", "", lineage_info$lineage)
+  lineage_info$lineage<-gsub("Cosmopolitan_", "", lineage_info$lineage)
+
   if(length(grep("\\.", clades)) != 0 ) {
     clades<-clades[-c(grep("\\.", clades))]
   }
